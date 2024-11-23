2f9bc17b4a2f34ca96ab2003bef9fc72ac41c34f
NotVann <130763613+NotVann@users.noreply.github.com>
2024-11-23 18:03:16 +0700

    Add files via upload

---
+++ Pattern-2.java
@@ -0,0 +1,12 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int count = 5;
+    
+        for (int i = count; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
